ad5f55c383acfb80f6676baff3774a84d8b34518
Darcylinh <darcylinhthai@gmail.com>
2024-06-30 13:55:36 +1000

    Update DAY9_SQL.sql

--- DAY9_SQL.sql
+++ DAY9_SQL.sql
@@ -13,8 +13,23 @@ select x,y,z,
 From Triangle
 
 ___câu 3
+SELECT
+  ROUND(
+   CAST(
+  (SUM(CASE WHEN call_category = 'n/a' or call_category is null THEN 1 ELSE 0 END)) * 100/ COUNT(*)
+AS DECIMAL),1)
+ AS uncategorised_call_pct
+ FROM callers
+  
 ___câu 4
 select name
 from Customer
 where referee_id !=2 or referee_id is null
+  
 ___câu 5
+select survived,
+ sum(case when pclass=1 then 1 else 0 end) as first_class,
+ sum(case when pclass=2 then 1 else 0 end) as second_class,
+ sum(case when pclass=3 then 1 else 0 end) as third_class
+from titanic
+group by survived
